17afecaea36729e88feabf3f03b2c4eb96326ed7
David Mahbubi <ulrichdavid0370@gmail.com>
2023-09-02 21:38:59 +0700

    Initial Commit

---
+++ Chess Timer/Chess_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chess_TimerApp.swift
+//  Chess Timer
+//
+//  Created by David Mahbubi on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Chess_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
